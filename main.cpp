f4943fce090c7816e14639a2054328824673c6c2
Kai Oliver <k.quambusch@gmx.de>
2010-01-02 22:13:26 +0100

    designing main

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    cout << "iBrute v0.1" << endl;
+    cout << "Which hashtype do you want to brute?" << endl;
+    cout << "(MD5, SHA128, SHA256, SHA512)> " << endl;
+
+}
